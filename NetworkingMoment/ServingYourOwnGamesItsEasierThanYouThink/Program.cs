fb46c762d7022b309986a28caa87c8247a17e2d6
leo-buono <56273373+leo-buono@users.noreply.github.com>
2022-03-25 20:58:03 -0400

    Added teh server solution

---
+++ NetworkingMoment/ServingYourOwnGamesItsEasierThanYouThink/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+//public class UDPServer
+//{
+//    public static void RunServer()
+//    {
+//        byte[] buffer = new byte[512];
+//        IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+//        IPAddress ip = host.AddressList[1];
+
+//        Console.WriteLine("Server name: {0}  IP: {1}", host.HostName, ip);
+//        IPEndPoint localEP = new IPEndPoint(ip, 11111);
+//        Socket server = new Socket(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+//        IPEndPoint client = new IPEndPoint(IPAddress.Any, 0); // 0 for any available port
+//        EndPoint remoteClient = (EndPoint)client;
+//        try
+//        {
+//            server.Bind(localEP);
+//            Console.WriteLine("Waiting for data...");
+//            while (true)
+//            {
+//                int rec = server.ReceiveFrom(buffer, ref remoteClient);
+//                Console.WriteLine("Client: {0}  | Data: {1}", remoteClient.ToString(), Encoding.ASCII.GetString(buffer, 0, rec));
+//                Console.WriteLine("Waiting for data...");
+//            }
+//            //server.Shutdown(SocketShutdown.Both);
+//            //server.Close();
+//        }
+//        catch (Exception e)
+//        {
+//            Console.WriteLine(e.ToString());
+//        }
+//    }
+//    public static int Main(String[] args)
+//    {
+//        RunServer();
+//        Console.Read();
+//        return 0;
+//    }
+//}
+
+public class TCPServer 
+{
+    public static void StartServer() 
+    {
+        Console.ReadKey();
+        byte[] buffer = new byte[512];
+        IPHostEntry hostInfo = Dns.GetHostEntry(Dns.GetHostName()); 
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+        IPEndPoint localEP = new IPEndPoint(ip, 11111);
+
+        Socket server = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        Socket UDPSocket  = new Socket(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+        //UDP
+        IPEndPoint client = new IPEndPoint(ip, 11112);
+        EndPoint remoteClient = (EndPoint)client;
+        try
+        {
+                server.Bind(localEP);
+                UDPSocket.Bind(client);
+                server.Listen(10);
+
+                Console.WriteLine("Waiting for Connection...");
+                Socket handler = server.Accept();
+                Console.WriteLine("Client Connected.");
+                IPEndPoint clientEP = (IPEndPoint)handler.RemoteEndPoint;
+                Console.WriteLine("Client {0} connected at port {1}", clientEP.Address, clientEP.Port);
+                //send data 
+                byte[] msg = Encoding.ASCII.GetBytes("Accepted");
+                handler.Send(msg);
+            server.Blocking = false;
+            UDPSocket.Blocking = false;
+            while (true)
+            {
+                //TCP
+                try
+                {
+                    int recieved = handler.Receive(buffer);
+
+                    Console.WriteLine("Recieved: {0}", Encoding.ASCII.GetString(buffer, 0, recieved));
+                }
+                catch (SocketException socke) 
+                {
+                    if (socke.SocketErrorCode != SocketError.WouldBlock)
+                    {
+                        Console.WriteLine("Socket Error");
+                    }
+                }
+                //UDP
+                //Clean the buffer so there is no garbage 
+                buffer = new byte[512];
+                try
+                {
+                    int rec = UDPSocket.ReceiveFrom(buffer, ref remoteClient);
+                    if (rec != 0)
+                    {
+                        //send the data back to the other computer
+                        Console.WriteLine("Client: {0}  | Data: {1}", remoteClient.ToString(), Encoding.ASCII.GetString(buffer, 0, rec));
+                        Console.WriteLine("Waiting for data...");
+
+                    }
+                }
+                catch (SocketException socke)
+                {
+                    if (socke.SocketErrorCode != SocketError.WouldBlock)
+                    {
+                        Console.WriteLine("Socket Error");
+                    }
+                }
+            }
+            //handler.Shutdown(SocketShutdown.Both);
+        }
+            catch (SocketException sockeet)
+            {
+                if (sockeet.SocketErrorCode != SocketError.WouldBlock)
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+    }
+    public static int Main(String[] args) 
+    {
+        StartServer();
+        Console.ReadKey();
+        return 0;
+    }
+}
+
+//namespace ServingYourOwnGamesItsEasierThanYouThink
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
